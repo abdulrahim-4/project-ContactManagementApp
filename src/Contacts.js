beff4ac82b52fc286789e472a8891e2d20585fa5
Abdul Rahim <abdulrahim42002@gmail.com>
2023-10-11 03:59:59 +0530

    3rd commit-responsive

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faTrashCan, faUser} from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+
 const Contacts = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -19,11 +20,11 @@ const Contacts = () => {
     }
 
     return (
-        <div>
-            <div className='m-5 border-x-4 border-y-4'>
-                <h1 className='flex justify-center p-3'>Contact Manager</h1>
-                <p className='mx-3 font-bold'>Add Contact</p>
-                <form className='mx-3'>
+        <div className="container mx-auto px-4">
+            <div className='my-5 border-x-4 border-y-4 p-5'>
+                <h1 className='text-center mb-3'>Contact Manager</h1>
+                <p className='font-bold mb-3 text-center'>Add Contact</p>
+                <form>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Name</label>
                         <input type="text" className="form-control" id="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
@@ -32,24 +33,24 @@ const Contacts = () => {
                         <label htmlFor="email" className="form-label">Email</label>
                         <input type="text" className="form-control" id="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
-                    <button className='btn btn-primary mb-3' onClick={addContact}>Add</button>
+                    <button className='btn btn-primary' onClick={addContact}>Add</button>
                 </form>
             </div>
-       
-            <div className='m-4'>
-                <h2>Contact List:</h2>
-                {contacts.map((contact, index) => (
-                    <div key={index} className='flex items-center mx-3'>
-                        <div>
-                        <FontAwesomeIcon icon={faUser} beatFade />
 
+            <div className='mx-4'>
+                <h2 className='mb-3'>Contact List:</h2>
+                {contacts.map((contact, index) => (
+                    <div key={index} className='flex items-center mb-3'>
+                        <div className='mr-3'>
+                            <FontAwesomeIcon icon={faUser} beatFade />
                         </div>
-                        <div className='flex flex-col relative ml-3 p-3 px-5'>
+                        <div className='flex flex-col'>
                             <li className='list-none'>{contact.name}</li>
                             <li className='list-none'>{contact.email}</li>
                         </div>
-                        <button className='btn btn-danger ml-auto' onClick={() => deleteContact(index)}><FontAwesomeIcon icon={faTrashCan} /></button>
-                        <hr />
+                        <button className='btn btn-danger ml-auto' onClick={() => deleteContact(index)}>
+                            <FontAwesomeIcon icon={faTrashAlt} />
+                        </button>
                     </div>
                 ))}
             </div>
